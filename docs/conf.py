e3189eacebe9f6b004d77547fabf1b64f26b9a3d
GloriaWYY <89892646+GloriaWYY@users.noreply.github.com>
2022-01-13 23:37:35 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u"eda-helper"
+project = u"gov_ca_gdp_helper"
 copyright = u"2022, Aldo Barros, Gabriel Fairbrother, Ramiro Mejia, Wanying Ye"
 author = u"Aldo Barros, Gabriel Fairbrother, Ramiro Mejia, Wanying Ye"
 
