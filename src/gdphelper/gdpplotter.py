90bf7c02797531d4edb159d5ed4eff90ca9c0c7e
Aldo S Barros <aldosaltao@gmail.com>
2022-01-19 09:26:31 -0800

    gdpplotter v02

--- src/gdphelper/gdpplotter.py
+++ src/gdphelper/gdpplotter.py
@@ -1,23 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-def gdpplotter(type_chart, type_feature):
+def gdpplotter(data_name, aggregation):
     """Plot a line chart of periods and the selected feature.
     
     Parameters
     ----------
-    type_chart : string
-        There are two types of chart, "all_together" or "split_charts".
-        The first type will bring up all sub-features in the same plot
-        The second type will bring up all one chart for each sub-feature.
-    type_feature : string
-        There are two types of features, "region" and "industry".
-        The first one will break down the GDP by each reageion.
-        The second one will break down the GDP by each industry.
-
+    data_name : string
+        This is the name of the dataset that you want to draw a chart.
+    aggregation : string
+        There are two types of aggregation, "canada" and "province".
+        The first one will give the GDP by total Canada.
+        The first one will give the GDP by province.
+        
     Returns
     -------
-        line chart of GDP by the periods within the dataset.
+        line chart of GDP by the periods.
 
     Examples
     --------
