c5acd7c51de874df15f146ae149c33a947835ed6
Aldo S Barros <aldosaltao@gmail.com>
2022-01-19 09:26:56 -0800

    gpdplotter v3

--- src/gdphelper/gdpplotter.py
+++ src/gdphelper/gdpplotter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import numpy as np
 
 def gdpplotter(data_name, aggregation):
     """Plot a line chart of periods and the selected feature.
@@ -19,6 +20,49 @@ def gdpplotter(data_name, aggregation):
 
     Examples
     --------
-    >>> 
-
+    >>>
     """
+    #reading the database within the function
+    file = f"{data_name}.csv"
+    df = pd.read_csv(file)
+    print("database loaded")
+    
+    #transforming the database to fit on matplolib's data structure.
+    #grouping by region
+    df_grouped_region = df.groupby(['REF_DATE','GEO']).agg(VALUE = ('VALUE', 'sum')).reset_index()
+    #pivoting
+    df_grouped_region_pivoted = pd.pivot_table(df_grouped_region, index=['REF_DATE'], values=['VALUE'], columns=['GEO'],fill_value=0, aggfunc=np.sum).reset_index()
+    #fixing columns
+    df_grouped_region_pivoted.columns = ['_'.join(col).strip() for col in df_grouped_region_pivoted.columns.values]
+    print("database was transformed")
+    
+    #selecting the province names
+    provinces = list(df_grouped_region_pivoted.columns)
+    provinces.remove('REF_DATE_')
+    provinces.remove('VALUE_Canada')
+    provinces
+    if provinces == []:
+        print("WARNING: checking province names: there aren't provinces in this database")
+        print("therefore, use only option 'canada' for 'aggragation'.")
+    else:
+        print("checking province names: we have provinces in this dataset")
+    
+    #drawing charst according to the selected option
+    if aggregation == "province":
+        #chart 01
+        df_grouped_region_pivoted.plot(x='REF_DATE_', y=provinces)
+        plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
+        plt.ylabel("GPD (CAD$ - millions)")
+        plt.xlabel("Years")
+        plt.title("GDP (CAD$ millions) VS Years", loc='center')
+        plt.show()
+
+    if aggregation == "canada":
+        #chart 02
+        df_grouped_region_pivoted.plot(x='REF_DATE_', y='VALUE_Canada')
+        plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
+        plt.ylabel("GPD (CAD$ - millions)")
+        plt.xlabel("Years")
+        plt.title("GDP (CAD$ millions) VS Years", loc='center')
+        plt.show()
+
