bc298b8b63226ac5119e9545cb5fd30498754d00
Gabe Fairbrother <gfairbrother@gmail.com>
2022-01-20 17:14:10 -0800

    Updated Test functions

--- tests/test_gdpcleaner.py
+++ tests/test_gdpcleaner.py
@@ -6,22 +6,27 @@ import pytest
 @pytest.fixture
 def input_frame():
    input_dataframe = pd.read_csv('example.csv')
-   return input_data_frame
+   return input_dataframe
 
 def test_correct_output():
     # TEST 1: Test that gdpcleaner returns a dataframe and a string
-    output_1, output_2 = gdpcleaner(input_frame)
+    output_1 = gdpcleaner(input_frame)
 
     assert type(output_1) == pd.DataFrame, "Output #1 is not a pandas DataFrame"
-    assert type(output_2) == str, "Output #2 is not a String"
-
-def test_correct_columns():
-    #TEST 2: Test that the dataframe has the appropriate column names
-    output_1, output_2 = gdpcleaner(input_frame)
-    expected_columns=['Date', 'Location', 'Prices', 'Scale', 'Value']
-    assertCountEqual(output.columns, expected_columns)
 
 def test_column_count():
-    #TEST 3: Test the dataframe has the correct column count
-    output_1, output_2 = gdpcleaner(input_frame)
-    assert len(output_1.columns) == 5, "Wrong number of columns"
+    #TEST 2: Test the dataframe has the correct column count
+    output_1 = gdpcleaner(input_frame)
+    assert len(output_1.columns) >= 5, "Wrong number of columns"
+
+def test_empty():
+    #TEST 3: Check the dataframe is not empty
+    output_1 = gdpcleaner(input_frame)
+    assert len(output_1) != 0, "Dataframe should not be empty!"
+
+def test_column_names():
+    #TEST 4: Any unaccounted for columns in output
+    output_1 = gdpcleaner(input_frame)
+
+    for c in output_1.columns:
+        assert c in any("Date", "Location", "Scale", "Unit", "Value", "NAICS_Class", "Industry", "Sub-sector", "Special_Industry"), "Unknown Column in output"
