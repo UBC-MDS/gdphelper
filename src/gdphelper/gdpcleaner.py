750aea7696a1eaba5fd242688ac09de803aa5775
Gabe Fairbrother <gfairbrother@gmail.com>
2022-01-20 16:05:05 -0800

    Latest function

--- src/gdphelper/gdpcleaner.py
+++ src/gdphelper/gdpcleaner.py
@@ -1,20 +1,36 @@
-def gdpcleaner(gdpdata: DataFrame):
-     """Load downloaded data, Remove spurious columns, Rename relevant columns, 
-        Scrub any string artifacts (eg [T008]), Identify category 
+import pandas as pd
+def gdpcleaner(gdpdata: pd.DataFrame):
+    """
+    Author: Gabe Fairbrother
+    Remove spurious columns, Rename relevant columns, Scrub any string artifacts (eg [T008])
 
 
     Parameters
     ----------
     gdpdata: DataFrame 
-        a loaded dataframe based on a downloaded Open Government GDP at basic prices dataset (https://open.canada.ca/en/open-data)
+    a loaded dataframe based on a downloaded Open Government GDP at basic prices dataset (https://open.canada.ca/en/open-data)
 
 
     Returns
     -------
-    (DataFrame, string):  A cleaned and simplified DataFrame of the relevant columns for summary and visualization, a string indicating category (such as percentage, industry, NAICS)
+    DataFrame:  A cleaned and simplified DataFrame of the relevant columns for summary and visualization
 
 
     Examples
     --------
-    >>> gdpcleaner(example_data)
+    #>>> result = gdpcleaner(example_data)
     """
+    #Check for DataFrame input argument
+    if (type(gdpdata == None)):
+        raise ValueError("Must specify a Pandas DataFrame")
+
+    if (type(gdpdata) != pd.DataFrame):
+        raise TypeError("Argument must be a Pandas DataFrame")
+
+    #Remove spurious columns
+    cleaned_frame = gdpdata.drop(columns=['DGUID', 'UOM', 'UOM_ID', 'SCALAR_ID', 'VECTOR', 'COORDINATE', 'STATUS', 'SYMBOL', 'TERMINATED', 'DECIMALS'])
+ 
+    #Drop any rows with null value
+    cleaned_frame = cleaned_frame.dropna()
+    
+    cleaned_frame = gdpdata.rename(columns={'REF_DAT': 'Date', 'GEO': 'Location', 'SCALAR_FACTOR': 'Scale', 'VALUE': 'Value', 'UOM': 'Unit'})
