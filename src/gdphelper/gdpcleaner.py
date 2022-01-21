de83adda3600cf18efca53ddf4b1450b0eb10836
Gabe Fairbrother <gfairbrother@gmail.com>
2022-01-20 20:11:50 -0800

    Updated data sources again

--- src/gdphelper/gdpcleaner.py
+++ src/gdphelper/gdpcleaner.py
@@ -30,26 +30,31 @@ def gdpcleaner(gdpdata: pd.DataFrame):
     #>>> result = gdpcleaner(example_data)
     """
     #Check for DataFrame input argument
-    if (type(gdpdata == None)):
-        raise ValueError("Must specify a Pandas DataFrame")
-
     if (type(gdpdata) != pd.DataFrame):
-        raise TypeError("Argument must be a Pandas DataFrame")
+         raise TypeError("Argument must be a Pandas DataFrame")
+
+    cleaned_frame = gdpdata
 
     #Remove spurious columns
-    cleaned_frame = gdpdata.drop(columns=['DGUID', 'UOM_ID', 'SCALAR_ID', 'VECTOR', 'COORDINATE', 
-                                          'STATUS', 'SYMBOL', 'TERMINATED', 'DECIMALS', 'Value', 'Seasonal adjustment'])
- 
+    spurious = ['DGUID', 'UOM_ID', 'SCALAR_ID', 'VECTOR', 'COORDINATE', 
+                                          'STATUS', 'SYMBOL', 'TERMINATED', 'DECIMALS', 'Value', 'Seasonal adjustment']
+    for column in cleaned_frame.columns :
+        if column in spurious:
+            cleaned_frame = cleaned_frame.drop(columns=column)
+
+
     #Drop any rows with null value
     cleaned_frame = cleaned_frame.dropna()
     
     #Rename relevant columns
-    cleaned_frame = gdpdata.rename(columns={'REF_DAT': 'Date', 'GEO': 'Location', 
+    cleaned_frame = cleaned_frame.rename(columns={'REF_DATE': 'Date', 'GEO': 'Location', 
                                             'SCALAR_FACTOR': 'Scale', 'VALUE': 'Value', 'UOM': 'Unit'})
 
-    for column in gdpdata.columns():
+    for column in cleaned_frame.columns:
         if 'NAICS' in column:
-            gdpdata.rename({column: 'NAICS_Class'})
-        if 'aggregat' in column: #Not a spelling mistake, there are two similar column headers in different datasets
-            gdpdata.rename({column: 'Special_Industry'})
-        
+
+            cleaned_frame = cleaned_frame.rename(columns={column: 'NAICS_Class'})
+        if 'aggregat' in column: #Not a spelling mistake, there are multiple similar column headers in different datasets
+            cleaned_frame = cleaned_frame.rename(columns={column: 'Special_Industry'})
+
+    return cleaned_frame        
